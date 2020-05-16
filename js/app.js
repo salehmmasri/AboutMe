01d326de4bafe429cdbac02e6c74391ab1794073
salehmmasri <salehalmasri494@yahoo.com>
2020-05-16 21:35:50 +0300

    final result

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ var major = prompt('my major is software eng').toLowerCase();
 console.log('major  ' + major);
 switch (major) {
 case 'yes':
-case 'y' :
+case 'y':
   total = total + 1;
   console.log('Major yes true');
   alert('your answer is true');
@@ -88,46 +88,38 @@ default:
 }
 alert('you got ' + total);
 alert('now lets play another game you should guss my age i will helpe you don\'t worry');
-var myAge=prompt('please enter the number');
+var myAge = prompt('please enter the number');
 var count;
-count=0;
+count = 0;
 do {
-  if(myAge==24){
+  if (myAge == 24) {
     alert('right answer');
-    count=9;
+    count = 9;
   }
-  else if(myAge>24){
+  else if (myAge > 24) {
     count++;
-    myAge=prompt('too high try again');
+    myAge = prompt('too high try again');
     console.log(count);
   }
-  else if(myAge<24){
+  else if (myAge < 24) {
     count++;
-    myAge=prompt('too low');
+    myAge = prompt('too low');
     console.log(count);
   }
-} while (count<=3);
-if(count>3 && count<8){
+} while (count <= 3);
+if (count > 3 && count < 8) {
   alert('the right answer is 24');
-  console.log('in if'+count);
+  console.log('in if' + count);
 }
-
-var arr=['bmw','kia','ferrari','lamborghini','mercedes'];
+var arr = ['bmw', 'kia', 'ferrari', 'lamborghini', 'mercedes'];
 alert('now the last one you should guess my favoret car');
 for (let i = 0; i < 6; i++) {
-  var guess=prompt('guess my favoret car').toLowerCase();
-  for (var j=0;j<arr.length;j++) {
-    var check=arr[j]==guess;
-    console.log(check);
-    if(check){
-      i=66;
+  var guess = prompt('guess my favoret car').toLowerCase();
+  for (var j = 0; j < arr.length; j++) {
+    if (arr[j] == guess) {
+      i = 66;
       break;
     }
 
   }
 }
-
-function x(){
-  console.log('saleh');
-}
-x();
